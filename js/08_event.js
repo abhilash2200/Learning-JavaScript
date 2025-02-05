f54b7ff77e899039ad429057fa8310eb84443b90
abhilash2200 <ab.digitalwolf@gmail.com>
2025-02-05 16:04:29 +0530

    learning event

---
+++ js/08_event.js
@@ -0,0 +1,31 @@
+/*
+*DATE - 05,Feb 25
+*/
+
+// mouse event
+/*
+* click
+* double click
+* right click
+* mouse hover
+* mouse out
+* mouse down
+* mouse up
+*/
+
+// 'click'	Jab user kisi element par click kare
+// 'dblclick'	Jab user double-click kare
+// 'mousedown'	Jab mouse ka button press ho
+// 'mouseup'	Jab mouse ka button release ho
+// 'mouseover'	Jab mouse kisi element ke upar aaye
+// 'mouseout'	Jab mouse kisi element se bahar jaye
+// 'mousemove'	Jab mouse move kare
+// 'contextmenu'	Jab user right-click kare
+
+// keyboard event
+
+/*
+* keydown
+* keyup
+* keypress
+*/
