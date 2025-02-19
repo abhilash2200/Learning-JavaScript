22b2133ec01fef5977d1aa5ecb5384c0aa28a13a
abhilash2200 <ab.digitalwolf@gmail.com>
2025-02-19 18:36:41 +0530

    learning array

--- js/09_loop.js
+++ js/09_loop.js
@@ -35,3 +35,25 @@ do{  //Loop tab tak chalega jab tak k ki value numbers.length se chhoti hai. Jai
     total2 += numbers[k];
     k++;
 }while(k < numbers.length);
+
+
+//break and continue
+
+//Example 1 continue
+for(let l = 0; l <= 15; l++){  //Loop tab tak chalega jab tak l ki value numbers.length se chhoti hai.
+    if(l == 11){
+        continue; // Jab l == 11 hoga, loop is iteration ko skip kar dega aur next iteration par chala jayega.
+    }
+    document.write(l); //0,1,2,3,4,5,6,7,8,9,10,12,13,14,15
+}
+
+//Example 2 break
+
+for(let m = 0; m <= 15; m++){  //Loop tab tak chalega jab tak m ki value numbers.length se chhoti hai.
+    if(m == 11){
+        break; // Jab m == 11 hoga, loop ko break kar dega, aur loop execution stop ho jayega.
+    }
+    document.write(m); //0,1,2,3,4,5,6,7,8,9,10
+}
+
+
